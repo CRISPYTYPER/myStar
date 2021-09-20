896a2d904e4fb11d1e1e7771ad922209b08f457b
CRISPYTYPER <jack3409@naver.com>
2021-09-20 16:45:21 +0900

    setted font and button

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,14 +1,35 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <h1 class="display-2 font-weight-bold mb-3" style="color: #C5CAE9;">
-          나만의
+  <v-container fill-height>
+    <v-row class="text-center" align="center" justify="center">
+      <v-col cols="12" class="mb-4 mt-4">
+        <h1 class="display-3 mb-3" id="title">
+          나만의 별 찾기
         </h1>
       </v-col>
+      <v-col cols="12" class="mb-4">
+        <div class="my-3">
+            <v-btn
+              color="error"
+              dark
+              large
+              id="startBtn"
+            >
+              시작하기
+            </v-btn>
+        </div>
+      </v-col>
     </v-row>
+    
   </v-container>
 </template>
-<style>
- 
+<style scoped>
+ #title {
+  color: #C5CAE9;
+  font-family: 'Nanum Pen Script', cursive !important;
+  font-weight: 500;
+ }
+#startBtn {
+  font-family: 'Jua', sans-serif !important;
+  font-size: 1.5rem;
+}
 </style>
