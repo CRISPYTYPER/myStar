07f0b1bec27509a4d0c8448fc3dfd3c29e5e3430
CRISPYTYPER <jack3409@naver.com>
2021-09-20 15:58:41 +0900

    placed background image

---
+++ src/components/Intro.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <h1 class="display-2 font-weight-bold mb-3" style="color: #C5CAE9;">
+          나만의
+        </h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<style>
+ 
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
 // router/index.js
 import Vue from "vue"
 import VueRouter from "vue-router"
-import HomeComponent from '@/components/HelloWorld'
+import IntroComponent from '@/components/Intro'
+import HelloComponent from '@/components/HelloWorld'
 // 뷰 어플리케이션에 라우터 플러그인을 추가한다.
 Vue.use(VueRouter)
 
 // 라우트 정보
 // 라우팅을 해주면서 #을 제거해주면서, path에 입력한 경로에 매핑된 컴포넌트를 보여준다.
 const route = [
-    {path: "/", component: HomeComponent}, // 루트(/)로 접속했을때, Home 컴포넌트를 보여준다.
+    {path: "/", component: IntroComponent}, // 루트(/)로 접속했을때, intro 컴포넌트를 보여준다.
+    {path: "/home", component: HelloComponent},
 ];
 
 // Vue 라우터 인스턴스 생성
